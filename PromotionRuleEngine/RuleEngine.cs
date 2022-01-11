9f7ed82dbc6c7ab1e1b60cceb9af6981d060d62c
sakalija <sakalija@DESKTOP-NECM80R>
2022-01-11 13:37:09 +0530

    Code Changes for promotion Engine

--- PromotionRuleEngine/RuleEngine.cs
+++ PromotionRuleEngine/RuleEngine.cs
@@ -1,8 +1,60 @@
-﻿using System;
+﻿using PromotionRuleEngine.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PromotionRuleEngine
 {
     public class RuleEngine
     {
+        private readonly IEnumerable<Product> Products;
+        private readonly IEnumerable<PromotionRule> PromotionRules;
+
+        public RuleEngine(IEnumerable<Product> products, IEnumerable<PromotionRule> promotionRules)
+        {
+            this.Products = products;
+            this.PromotionRules = promotionRules;
+        }
+
+        public void CheckOutCartItems(Order order)
+        {
+            var promotionRulesForOrder = getPromotionRulesForOrder(order);
+            calculatePriceForOrder(order);
+            applyPromotion(order, promotionRulesForOrder);
+        }
+
+        private void applyPromotion(Order order, IList<PromotionRule> promotionRulesForOrder)
+        {
+            throw new NotImplementedException();
+        }       
+
+        private void calculatePriceForOrder(Order order)
+        {
+            foreach (var item in order.CartItems)
+            {
+                var itemPrice = Products.FirstOrDefault(x => x.Id == item.Id).Price;
+                var quantity = item.Quantity;
+                if (quantity > 0)
+                    order.TotalAmt += quantity * itemPrice;
+            }
+        }
+
+        private IList<PromotionRule> getPromotionRulesForOrder(Order order)
+        {            
+
+            var rules = new List<PromotionRule>();
+
+            Parallel.ForEach(order.CartItems, cartItem =>
+            {
+                var rule = PromotionRules.Where(pr => pr.CartItems.Any(c => c.Id == cartItem.Id));
+                if (rule != null)
+                {
+                    rules.AddRange(rule);
+                }
+            });
+            return rules;
+        }
+
     }
 }
